b6e57dbe96e528d12ec43f5905656bc228f4efd2
Amit Rao <amit.raao04@gmail.com>
2025-04-12 14:13:30 +0530

    Added 20 musical instrument products with images stored in public/images

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
+app.use('/images', express.static('public/images'));
+
 
 // Test route
 app.get('/', (req, res) => {
